22a784d5bd4866dc1c40905d7f5fb11ff231cf40
yonix <yonix4423@gmail.com>
2016-02-01 18:01:13 +0900

    Tofu create.

--- kadai-20160201/my_program.py
+++ kadai-20160201/my_program.py
@@ -2,17 +2,33 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 
-#mc = minecraft.Minecraft.create("cvcis01.progrape.jp")
-mc = minecraft.Minecraft.create("bes-master.cis.nagasaki-u.ac.jp")
-pos = mc.player.getPos()
-#mc.postToChat(pos)
-#mc.postToChat("Hello Python by Hayashida")
+def Wait():
+    for i in range(0, 100):
+        print ""
 
+mc = minecraft.Minecraft.create("cvcis01.progrape.jp")
+#mc = minecraft.Minecraft.create("bes-master.cis.nagasaki-u.ac.jp")
+pos = mc.player.getPos()
+#pos = [-1931, 4, -898]
+mc.postToChat(pos)
+
+#mc.player.setTilePos(1000,0,1000)
+
+
+for x in range(0, 10):
+    for y in range(0, 10):
+        for z in range(0, 10):
+            #mc.postToChat("%d, %d, %d" % (x, y, z))
+            mc.setBlock(pos.x+x, pos.y+z, pos.z+y, block.STONE.id)
+            Wait()
+
+
+'''
 for i in range(0, 10):
     for j in range(0, 10):
         for k in range(0, 10):
 		mc.setBlock(pos.x+i, pos.y + j, pos.z+k, block.AIR.id)
-'''
+
 for i in range(1, 5):
     for j in range(0, 5):
         for k in range(1, 5):
